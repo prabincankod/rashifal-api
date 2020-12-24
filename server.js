0338c148f6a45f724ebe859c715fbf897b2b1326
prabin21 <prabinsubedi2016@gmail.com>
2020-12-24 20:54:21 +0545

    ful

--- server.js
+++ server.js
@@ -77,4 +77,6 @@ resp.send(obj);
 });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
