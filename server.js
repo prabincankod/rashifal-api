38a4f38cb14b3b642cd254248ddb082eabceaf23
Glitch (rashifal-api) <none>
2020-12-24 11:12:08 +0000

    🍚☕️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,78 +1,81 @@
 const express = require("express");
 const app = express();
-const request = require("request");
+const cors = require("cors");
+const axios = require("axios");
 const cheerio = require("cheerio");
+app.use(cors());
 
 app.get("/", (request, response) => {
-  response.send("googal");
+  response.send("imma done ");
 });
 
-app.get("/:sign", (req, res) => {
-  var n;
-  var e = req.params.sign;
 
-  switch (e) {
-    case "aries":
-      n = "mesh";
-      break;
-    case "taurus":
-      n = "brish";
-      break;
-    case "gemini":
-      n = "mithun";
-      break;
-    case "cancer":
-      n = "karkat";
-      break;
-    case "leo":
-      n = "singha";
-      break;
-    case "virgo":
-      n = "kanya";
-      break;
-    case "libra":
-      n = "tula";
-      break;
-    case "scorpio":
-      n = "brischik";
-      break;
-    case "sagittarius":
-      n = "dhanu";
-      break;
-    case "capricorn":
-      n = "makar";
-    case "aquarius":
-      n = "kumbha";
-      break;
-    case "pisces":
-      n = "meen";
-      break;
+app.get("/:sign",(req,resp) => {
+  
+var n;
+var e = req.params.sign;
 
-      break;
-    default:
-      n = "I have never heard of that fruit...";
-  }
-  console.log(n);
+switch(e) {
+  case "aries":
+    n= "mesh";
+    break;
+  case "taurus":
+    n= "brish";
+    break;
+  case "gemini":
+    n = "mithun";
+    break;
+    case "cancer":
+    n= "karkat";
+    break;
+    case "leo":
+    n= "singha";
+    break;
+    case "virgo":
+    n="kanya";
+    break;
+    case "libra":
+    n= "tula";
+    break;
+case "scorpio":
+    n= "brischik";
+    break;
+case "sagittarius":
+    n= "dhanu";
+    break;
+case "capricorn":
+    n= "makar";
+    case "aquarius":
+    n= "kumbha";
+    break;
+    case "pisces":
+    n= "meen";
+    break;
 
-  request("https://www.hamropatro.com/rashifal/daily/" + n),
-    (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const prediction = $(".desc")
+}
+console.log(n)
+
+
+
+
+  const url = "https://www.hamropatro.com/rashifal/daily/"+n;
+console.log(url);
+axios.get(url).then((res) =>{
+    
+        const $ = cheerio.load(res.data);
+        const desc= $(".desc")
           .find("p")
           .text();
-        console.log(prediction);
+        console.log(desc);
+const obj = {
+"sun_sign":n,
+"prediction":desc
 
-        const obj = {
-          sun_sign: "Leo",
-          desc: prediction
-        };
-        console.log(obj);
-        res.send(obj);
-      }
-    };
+}
+resp.send(obj);
+  
 });
-
+});
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
