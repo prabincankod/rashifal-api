5eb37b60636be35956acee90b34c32479212ea07
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-01-12 23:25:14 +0545

    Wahi galti

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ resp.send(obj);
   
 });
 });
-app.listen(3000, () => {
-  console.log("App listening at https://rashifal-api.herokuapp.com")
-})
+app.listen(process.env.PORT || 3000, function(){
+ console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
