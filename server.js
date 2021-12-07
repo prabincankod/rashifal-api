14e2fc42b1f53761dcff18ae80e7d5bdbad4782f
Protam.js <prabinsubedi2016@gmail.com>
2021-12-07 17:54:30 +0545

    refactored some lines of code

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3000;
 var infobj = {
   Author: "PrabinSubedi",
   github_repo: "https://github.com/prabincodes/rashifal-api",
-  ip_address: "working on it"
+  ip_address: "working on it",
 };
 
 app.get("/", (request, response) => {
@@ -19,20 +19,20 @@ app.get("/", (request, response) => {
 app.get("/info", (request, response) => {
   response.send(infobj);
 });
-app.get("/hehe",(req,res)=>{
-	res.send("bipin")
+app.get("/hehe", (req, res) => {
+  res.send("bipin");
 });
 
-
-
 app.get("/api/:span/:sign", (req, resp) => {
-  var n;
-  var e = req.params.sign;
-  var f = req.params.span;
+  const spans = ["weekly", "monthly", "yearly"];
+
+  let n;
+  const e = req.params.sign;
+  const f = req.params.span;
   const obj1 = {
-    message: "/api/:span can only have daily, weekly, monthly or yearly"
-  }
-  if (!spans.includes(f.toLowerCase())) return resp.send(obj1)
+    message: "/api/:span can only have daily, weekly, monthly or yearly",
+  };
+  if (!spans.includes(f.toLowerCase())) return resp.send(obj1);
   else {
     switch (e) {
       case "aries":
@@ -81,123 +81,54 @@ app.get("/api/:span/:sign", (req, resp) => {
     if ((e = n)) {
       const url = `https://www.hamropatro.com/rashifal/${f}/${n}`;
       console.log(url);
-      axios.get(url).then(res => {
+      axios.get(url).then((res) => {
         const $ = cheerio.load(res.data);
-        const desc = $(".desc")
-          .find("p")
-          .text();
-        const date = $(".articleTitleNew")
-          .find("span")
-          .text();
+        const desc = $(".desc").find("p").text();
+        const date = $(".articleTitleNew").find("span").text();
         const dt = date.split(" ");
 
         const sc = desc.replace("\n", "");
         const obj = {
-          date: f === "daily" ? `${dt[2]} ${dt[1]} ${dt[0]} ${dt[3]}` : f === "weekly" ? `${dt[1]} - ${dt[3]} ${dt[0]}` : f === "monthly" ? `${dt[0]} ${dt[1]}` : `${dt[0]} ${dt[1]} ${dt[2]}`,
+          date:
+            f === "daily"
+              ? `${dt[2]} ${dt[1]} ${dt[0]} ${dt[3]}`
+              : f === "weekly"
+              ? `${dt[1]} - ${dt[3]} ${dt[0]}`
+              : f === "monthly"
+              ? `${dt[0]} ${dt[1]}`
+              : `${dt[0]} ${dt[1]} ${dt[2]}`,
           sun_sign: dt[dt.length - 5],
           prediction: sc,
-          ip: aip[0]
+          ip: aip[0],
         };
         resp.send(obj);
       });
     } else {
       const failobj = {
-        message: req.params.sign + " is meant to be a zodiac sign"
+        message: req.params.sign + " is meant to be a zodiac sign",
       };
 
       resp.send(failobj);
     }
   }
-
 });
+app.get("/all", async (req, res) => {
+  const response = await axios("https://www.hamropatro.com/rashifal/daily/");
+  const $ = cheerio.load(response.data);
 
-app.get("/all", (req, resp) => {
-  const url = "https://www.hamropatro.com/rashifal/daily/";
-  axios.get(url).then(res => {
-    const $ = cheerio.load(res.data);
+  let da = [];
 
-    const da = [
-      {
-        sunsign: $("h3")
-          .eq(0)
-          .text(),
-        prediction: $(".desc").find("p").eq(0).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(1)
-          .text(),
-        prediction: $(".desc").find("p").eq(1).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(2)
-          .text(),
-        prediction: $(".desc").find("p").eq(2).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(3)
-          .text(),
-        prediction: $(".desc").find("p").eq(3).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(4)
-          .text(),
-        prediction: $(".desc").find("p").eq(4).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(5)
-          .text(),
-        prediction: $(".desc").find("p").eq(5).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(6)
-          .text(),
-        prediction: $(".desc").find("p").eq(6).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(7)
-          .text(),
-        prediction: $(".desc").find("p").eq(7).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(8)
-          .text(),
-        prediction: $(".desc").find("p").eq(8).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(9)
-          .text(),
-        prediction: $(".desc").find("p").eq(9).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(10)
-          .text(),
-        prediction: $(".desc").find("p").eq(10).text().split(")",2)[1]
-      },
-      {
-        sunsign: $("h3")
-          .eq(11)
-          .text(),
-        prediction: $(".desc").find("p").eq(11).text().split(")",2)[1]
-      }
-    ];
+  for (let i = 0; i < 12; i++) {
+    const data = {
+      sunsign: $("h3").eq(i).text(),
+      prediction: $(".desc").find("p").eq(i).text().split(")", 2)[1],
+    };
 
-    resp.send(da);
-  });
+    da.push(data);
+  }
+  res.send(da);
 });
-
-
-
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express serve r listening on port %d in %s mode",
     this.address().port,
