7c0c1b8ee5e2130e3f6b57aa93bb13134e43c4d8
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-04-24 18:18:59 +0000

    wip: code cleanup.

--- api/api.ts
+++ api/api.ts
@@ -3,86 +3,95 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 export default async (req: VercelRequest, response: VercelResponse) => {
-  const spaen: Array<string> = ["daily", "weekly", "monthly", "yearly"];
+  const spans = ["daily", "weekly", "monthly", "yearly"];
 
-  let n;
+  let nepaliSign;
 
   const { span, sign } = req.query;
-  const obj1 = {
-    message: "/api/:span can only have daily, weekly, monthly or yearly",
-  };
-  if (!spaen.includes(span as string)) return response.send(obj1);
-  else {
-    switch (sign) {
-      case "aries":
-        n = "mesh";
-        break;
-      case "taurus":
-        n = "brish";
-        break;
-      case "gemini":
-        n = "mithun";
-        break;
-      case "cancer":
-        n = "karkat";
-        break;
-      case "leo":
-        n = "singha";
-        break;
-      case "virgo":
-        n = "kanya";
-        break;
-      case "libra":
-        n = "tula";
-        break;
-      case "scorpio":
-        n = "brischik";
-        break;
-      case "sagittarius":
-        n = "dhanu";
-        break;
-      case "capricorn":
-        n = "makar";
-        break;
-      case "aquarius":
-        n = "kumbha";
-        break;
-      case "pisces":
-        n = "meen";
-        break;
-    }
 
-    try {
-      const url = `https://www.hamropatro.com/rashifal/${span}/${n}`;
-      console.log(url);
-      axios.get(url).then((res) => {
-        const $ = cheerio.load(res.data);
-        const desc = $(".desc").find("p").text();
-        const date = $(".articleTitleNew").find("span").text();
-        const dt = date.split(" ");
 
-        const sc = desc.replace("\n", "");
-        const obj = {
-          date:
-            span === "daily"
-              ? `${dt[2]} ${dt[1]} ${dt[0]} ${dt[3]}`
-              : span === "weekly"
-              ? `${dt[1]} - ${dt[3]} ${dt[0]}`
-              : span === "monthly"
-              ? `${dt[0]} ${dt[1]}`
-              : `${dt[0]} ${dt[1]} ${dt[2]}`,
-          sun_sign: dt[dt.length - 5],
-          prediction: sc,
+
+
+  if (!spans.includes(span as string)) {
+    return response.send({
+      message: "/api/:span can only have daily, weekly, monthly or yearly",
+    });
+  }
+
+
+
+  switch (sign) {
+    case "aries":
+      nepaliSign = "mesh";
+      break;
+    case "taurus":
+      nepaliSign = "brish";
+      break;
+    case "gemini":
+      nepaliSign = "mithun";
+      break;
+    case "cancer":
+      nepaliSign = "karkat";
+      break;
+    case "leo":
+      nepaliSign = "singha";
+      break;
+    case "virgo":
+      nepaliSign = "kanya";
+      break;
+    case "libra":
+      nepaliSign = "tula";
+      break;
+    case "scorpio":
+      nepaliSign = "brischik";
+      break;
+    case "sagittarius":
+      nepaliSign = "dhanu";
+      break;
+    case "capricorn":
+      nepaliSign = "makar";
+      break;
+    case "aquarius":
+      nepaliSign = "kumbha";
+      break;
+    case "pisces":
+      nepaliSign = "meen";
+      break;
+    default:
+      return response.send({ "message": "please enter a valid sun sign" })
+      break;
+
+      try {
+        const url = `https://www.hamropatro.com/rashifal/${span}/${nepaliSign}`;
+        console.log(url);
+        axios.get(url).then((res) => {
+          const $ = cheerio.load(res.data);
+          const desc = $(".desc").find("p").text();
+          const date = $(".articleTitleNew").find("span").text();
+          const dt = date.split(" ");
+
+          const sc = desc.replace("\n", "");
+          const obj = {
+            date:
+              span === "daily"
+                ? `${dt[2]} ${dt[1]} ${dt[0]} ${dt[3]}`
+                : span === "weekly"
+                  ? `${dt[1]} - ${dt[3]} ${dt[0]}`
+                  : span === "monthly"
+                    ? `${dt[0]} ${dt[1]}`
+                    : `${dt[0]} ${dt[1]} ${dt[2]}`,
+            sun_sign: dt[dt.length - 5],
+            prediction: sc,
+          };
+          response.send(obj);
+        });
+      } catch {
+        const failobj = {
+          message: sign + " is meant to be a zodiac sign",
+          other: nepaliSign,
         };
-        response.send(obj);
-      });
-    } catch {
-      const failobj = {
-        message: sign + " is meant to be a zodiac sign",
-        other: n,
-      };
 
-      response.send(failobj);
-    }
+        response.send(failobj);
+      }
   }
 };
